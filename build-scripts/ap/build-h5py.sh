a67ce9a199d31dcf72dbe68f5e473a88400575f1
Dean Hidas <dhidas@submit1.nsls2.bnl.gov>
2021-02-06 14:25:25 -0500

    h5py update, gcc10 build script added

--- build-scripts/ap/build-h5py.sh
+++ build-scripts/ap/build-h5py.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
 
+pyver=3.9.1
+
 # h5py - parallel version: https://docs.h5py.org/en/stable/mpi.html
 # https://pypi.org/project/h5py/
-module load gcc/9.3.0 python/3.9.1 mpich/3.3.2-gcc-9.3.0 hdf5/1.12.0
+module load accelerator/path gcc/9.3.0 python/$pyver mpich/3.3.2-gcc-9.3.0 hdf5/1.12.0
 mkdir -pv /nsls2/software/ap/hdf5/build/h5py
 cd /nsls2/software/ap/hdf5/build/h5py
-wget https://files.pythonhosted.org/packages/a7/81/20d5d994c91ed8347efda90d32c396ea28254fd8eb9e071e28ee5700ffd5/h5py-3.1.0.tar.gz
+#wget https://files.pythonhosted.org/packages/a7/81/20d5d994c91ed8347efda90d32c396ea28254fd8eb9e071e28ee5700ffd5/h5py-3.1.0.tar.gz
+rm -rf h5py-3.1.0
 tar zxf h5py-3.1.0.tar.gz
 cd h5py-3.1.0
 export CC=mpicc
