5b057c2ce8868c4c1ea83317cf7aa8fa562f0a81
Dean Andrew Hidas <dhidas@bnl.gov>
2021-03-26 16:20:19 -0400

    use install config for texlive

--- build-scripts/ap/build-texlive.sh
+++ build-scripts/ap/build-texlive.sh
@@ -3,10 +3,23 @@
 # Latex
 # https://www.tug.org/texlive/build.html
 # Xaw header files were needed for this so I built the whole thing, required for this
-module load gcc/9.3.0 Xaw/1.0.13
+module load accelerator/path gcc/9.3.0
 mkdir -pv /nsls2/software/ap/texlive/build
 cd /nsls2/software/ap/texlive/build
-wget http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2020/texlive-20200406-source.tar.xz
-tar xf texlive-20200406-source.tar.xz
-cd texlive-20200406-source/
-TL_INSTALL_DEST=/nsls2/software/ap/texlive/20200406 ./Build
+wget http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2020/install-tl-unx.tar.gz
+tar zxf install-tl-unx.tar.gz
+cd install-tl-20200406
+
+cat <<EOF > texlive.profile
+TEXDIR /nsls2/software/ap/texlive/20200406
+TEXMFCONFIG ~/.texlive2020/texmf-config
+TEXMFHOME ~/texmf
+TEXMFLOCAL /nsls2/software/ap/texlive/20200406/texmf-local
+TEXMFSYSCONFIG /nsls2/software/ap/texlive/20200406/texmf-config
+TEXMFSYSVAR /nsls2/software/ap/texlive/20200406/texmf-var
+TEXMFVAR ~/.texlive2020/texmf-var
+EOF
+
+./install-tl -profile texlive.profile
+
+
