70f6e5bfc0b031ab943155f120e3b6e37978c296
Dean Andrew Hidas <dhidas@bnl.gov>
2021-11-02 12:39:04 -0400

    adding echo3d

---
+++ build-scripts/ap/build-echo3d.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# Get the location of zip file from someone in email or the web.
+
+module purge
+module load accelerator/path gcc/9.3.0
+
+NAME=ECHO3D_beta03
+mkdir -pv /nsls2/software/ap/echo3d/build
+cd /nsls2/software/ap/echo3d/build
+wget https://www.desy.de/fel-beam/s2e/data/${NAME}.zip
+unzip ${NAME}.zip
+VER=`echo '' | ./${NAME}/Codes/Linux/ECHO3D | head -1 | grep -oP 'ver \K\d+.\d+.\d+'`
+echo "installing version ${VER}"
+mv ${NAME} /nsls2/software/ap/echo3d/${VER}
+rm -rf __MACOSX
